9b156aa348ddc70eded5c5603c4ef10a2d9942ac
Mathieu Deschamps <153502859+Degaullus@users.noreply.github.com>
2024-03-25 15:30:24 +0100

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,44 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.background}>
-      <div>
-        <p>Footer</p>
-      </div>
+        <div className={styles.footer}>
+        
+    
+             <div className={styles.upper}>
+                <div className={styles.left}>
+                    <h2 className={styles.title}>THE COMPANY</h2>
+                    <p>New designs</p>
+                    <p>Weekly Insights</p>
+                    <p>Collaborations</p>
+                </div>
+                <div className={styles.left}>
+                    <h2 className={styles.title}>THE TEAM</h2>
+                    <p>Meet the Team</p>
+                    <p>OUr designers</p>
+                    <p>The Principal Shit</p>
+                </div>
+                <div className={styles.left}>
+                    <h2 className={styles.title} >OUR DESIGNS</h2>
+                    <p>Intentionnal ressources</p>
+                    <p>Digital Solutions</p>
+                    <p>Legal</p>
+                </div>
+                <div className={styles.left}>
+                    <h2 className={styles.title}>LEGAL SHIT</h2>
+                    <p>Meet the Team</p>
+                    <p>OUr designers</p>
+                    <p>The Principal Shit</p>
+                </div>
+
+             </div>
+             <div className={styles.downer}>
+          
+                  <p><i>Copyright 2024 Fashion with All Right Reserved by WARHAMMER</i></p>
+             </div>
+
+      
+        </div>
+     
     </div>
   );
 }
