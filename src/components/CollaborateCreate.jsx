e024ac8d6d8b58006e8dfbcb36e1dae244c9ebd6
Mathieu Deschamps <153502859+Degaullus@users.noreply.github.com>
2024-03-25 15:45:30 +0100

    last part baby

--- src/components/CollaborateCreate.jsx
+++ src/components/CollaborateCreate.jsx
@@ -3,8 +3,17 @@ import styles from "./CollaborateCreate.module.css";
 export default function CollaborateCreate() {
   return (
     <div className={styles.background}>
-      <div>
-        <p>CollaborateCreate</p>
+      <div className={styles.container}>
+        <div className={styles.titlesBox}>
+            <h2 className={styles.titles2}>THE MAD HATTERS©</h2>
+           <h1 className={styles.titles}>COLLABORATE.</h1>
+           <h1 className={styles.titles}>CREATE.</h1>
+           <h1 className={styles.titles}>INNOVATE.</h1>
+        </div>
+        <div >
+          <p className={styles.right}>The focus is in reating bullshit but I don't have anytime to correct the typo I made. Sorry if you read this, but it's kind of stressy day again here. How was it for you Mate ? Gonna be tuff this last 10 minutes . SHIT </p>
+        </div>
+        
       </div>
     </div>
   );
