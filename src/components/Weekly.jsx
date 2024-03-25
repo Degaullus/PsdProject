46d1384a6877b444d665bd34dd4fdf4ccef6bba3
Mathieu Deschamps <153502859+Degaullus@users.noreply.github.com>
2024-03-25 14:59:10 +0100

    add weekly

--- src/components/Weekly.jsx
+++ src/components/Weekly.jsx
@@ -2,8 +2,24 @@ import styles from "./Weekly.module.css";
 
 export default function Weekly() {
   return (
+    
     <div className={styles.background}>
-      <p>Weekly</p>
+
+        <div className={styles.container}>
+            <div className={styles.weekly}>
+                <h1 className={styles.weeklyTitle}>WEEKLY</h1>
+                <h1 className={styles.weeklyTitle2}>INSIGHTS</h1>
+            </div>
+            <div className={styles.subContainer}>
+                <p className={styles.dot2}>OO</p>
+        {/*     <img className={styles.dot} src="public/Weiß-groß.PNG" alt="" /> */}
+
+                 <p className={styles.para} >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 <img className={styles.frau} src="public/Frau.PNG" alt="" />
+            </div>
+
+        </div>
+      
     </div>
   );
 }
