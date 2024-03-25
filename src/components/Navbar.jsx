b67f4ba358955bf99218cfda689d76ea82a27112
Mathieu Deschamps <153502859+Degaullus@users.noreply.github.com>
2024-03-25 11:48:04 +0100

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import styles from "./Navbar.module.css"
+
 export default function Navbar() {
 
    
         return (
-          <nav>
-            <div >
-              <a href="#">The Mad Hatter</a>
+          <nav >
+            <div className={styles.navBar} >
+
               <ul>
+                <li>              
+                    <a href="#" >The Mad Hatter</a></li>
                 <li>
                   <a href="#home">Home</a>
                 </li>
